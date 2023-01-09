847ea5c8ae598d515c02744a7fe6ce3c356cd681
Mark Linn <marktlinn@gmail.com>
2023-01-09 15:01:14 +0100

    chore: set-up client-side app with tailwind and core dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-grotto h-screen flex flex-col justify-center items-center ">
+      <h1 className="font-title text-ivory">Hello World</h1>
+      <p className="font-body text-baby">Ready to go!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#265B8B",
+        ivory: "#ECECEB",
+        baby: "#84C7F2",
+        grotto: "#1181C8",
+        winkle: "#BFC7F2",
+      },
+      fontFamily: {
+        title: ["Quicksand", "Open Sans"],
+        body: ["Open Sans", "Avenir"],
+      },
+    },
+  },
+  plugins: [],
+};
