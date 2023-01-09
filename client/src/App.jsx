2174ef464cbe3e412ee21794ff700d8479200c8e
Mark Linn <marktlinn@gmail.com>
2023-01-09 15:06:40 +0100

    placed the app inside react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="bg-grotto h-screen flex flex-col justify-center items-center ">
-      <h1 className="font-title text-ivory">Hello World</h1>
-      <p className="font-body text-baby">Ready to go!</p>
-    </div>
+    <Router>
+      <div className="bg-grotto h-screen flex flex-col justify-center items-center ">
+        <h1 className="font-title text-ivory">Hello World</h1>
+        <p className="font-body text-baby">Ready to go!</p>
+      </div>
+    </Router>
   );
 }
 
