c247bba461c373879dccad8068a602ff3dfe54f5
Mark Linn <marktlinn@gmail.com>
2023-01-16 13:19:26 +0100

    fixes: for the cars layout

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -3,17 +3,17 @@ import rocket from "../assets/landing/rocket.png";
 
 const Cards = () => {
   return (
-    <div className="flex flex-col md:flex-row lg:flex-row lg:justify-around lg:p-30">
-      <div className="w-full bg-winkle lg:rounded-lg lg:w-2/6 ">
+    <div className="flex flex-col md:flex-row justify-around md:justify-center md:gap-4 items-center gap-20 p-4">
+      <div className=" bg-winkle md:h-[41rem] w-[80%] rounded-lg lg:w-2/6 flex flex-col items-center justify-around p-4">
         <img
-          className="mx-auto max-w-xs"
+          className="mx-auto max-w-[200px] sm:sm:max-w-[280px]"
           src={bookBoy}
           alt="boy sitting on book"
         />
-        <h4 className="text-xl text-center font-bold">
+        <h4 className="text-xl text-center font-bold text-primary">
           Participate from anywhere
         </h4>
-        <p className="p-5 leading-6">
+        <p className="p-5 leading-6 sm:max-w-full text-center text-primary">
           We believe that diversity and inclusivity are key to building a strong
           and vibrant community. That's why we welcome members from all walks of
           life and all corners of the globe. So come and join us, from where
@@ -25,16 +25,16 @@ const Cards = () => {
           </button>
         </div>
       </div>
-      <div className="w-full h-screen bg-rocketBg lg:rounded-lg lg:w-2/6">
+      <div className=" bg-rocketBg md:h-[41rem] w-[80%] rounded-lg lg:w-2/6 flex flex-col items-center justify-around p-4">
         <img
-          className="mx-auto mt-10 max-w-[300px]"
+          className="mx-auto mt-10 max-w-[200px] sm:max-w-[280px]"
           src={rocket}
           alt="a rocket ship blasting off"
         />
         <h4 className="text-xl text-center font-bold text-ivory mt-5">
           Ready to Go?
         </h4>
-        <p className="text-ivory leading-6 p-5">
+        <p className="text-ivory leading-6 p-5 text-center">
           Come on in and see what skills people are sharing. Like what you see?
           Sign up, join the community and learn what you’ve always wanted to!
         </p>
