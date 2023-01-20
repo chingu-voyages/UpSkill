8106fa44e10070ed7a9616d430be69e5e359e675
Dana Nhem <dananhem@gmail.com>
2023-01-20 21:01:58 +0100

    reintroduce original env vars

--- server/config/supabase.js
+++ server/config/supabase.js
@@ -1,10 +1,7 @@
 const { createClient } = require("@supabase/supabase-js");
-// TODO: delete these
-const supabaseUrl = process.env.supabaseUrl;
-const supabaseKey = process.env.supabaseKey;
-// TODO: keep these
-// const supabaseUrl = process.env.SUPABASE_PROJECT_URL;
-// const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+const supabaseUrl = process.env.SUPABASE_PROJECT_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const SECRET_KEY = process.env.SECRET_KEY;
 const BCRYPT_WF = 12;
 const supabase = createClient(supabaseUrl, supabaseKey);
