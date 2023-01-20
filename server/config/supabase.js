50ebafb8ec318b6e1a6f2a1d0a654ce716333f0c
Dana Nhem <dananhem@gmail.com>
2023-01-20 20:57:38 +0100

    feature: authenticate signup & login routes returns jwt

--- server/config/supabase.js
+++ server/config/supabase.js
@@ -1,6 +1,11 @@
 const { createClient } = require("@supabase/supabase-js");
-
-const supabaseUrl = process.env.SUPABASE_PROJECT_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+// TODO: delete these
+const supabaseUrl = process.env.supabaseUrl;
+const supabaseKey = process.env.supabaseKey;
+// TODO: keep these
+// const supabaseUrl = process.env.SUPABASE_PROJECT_URL;
+// const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const SECRET_KEY = process.env.SECRET_KEY;
+const BCRYPT_WF = 12;
 const supabase = createClient(supabaseUrl, supabaseKey);
-module.exports = { supabase };
+module.exports = { supabase, BCRYPT_WF, SECRET_KEY };
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,13 +1,85 @@
-const { supabase } = require("../config/supabase");
+const { supabase, BCRYPT_WF, SECRET_KEY } = require("../config/supabase");
+const { createToken } = require("../helpers/jwt");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-// Book a lesson
-const signup = async (req, res) => {
-  return res(200).json({ testing });
+const signup = async (req, res, next) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+
+    // Check for duplicate user
+    const { data: duplicateUser } = await supabase
+      .from("User")
+      .select("email")
+      .eq("email", email);
+    if (duplicateUser[0]) {
+      return res
+        .status(500)
+        .json({ Error: "User with this email already exists." });
+    }
+
+    // Hash password & insert user credentials in User table
+    const hashedPw = await bcrypt.hash(password, BCRYPT_WF);
+    const { data, error } = await supabase
+      .from("User")
+      .insert({
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        password: hashedPw,
+      })
+      .select("first_name, last_name, email, id");
+    const user = data[0];
+    // Handle missing inputs
+    if (error && error.code === "23502") {
+      return res.status(500).json({ Error: "Missing Credentials." });
+    }
+
+    // Create JWT 🥇
+    const token = createToken(user);
+
+    /*
+      Subsequently insert same id
+      from new user to userId column in User_data table
+    */
+    await supabase.from("User_data").insert({
+      userId: user.id,
+    });
+    return res.status(203).json({ Message: "Successfully registered.", token });
+  } catch (err) {
+    return next(err);
+  }
 };
 
-// Cancel a lesson
-const login = async (req, res) => {
-  return res(200).json({ testing });
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // 🔎 locate user
+    const { data } = await supabase
+      .from("User")
+      .select("email, password, id")
+      .eq("email", email);
+    const user = data[0];
+
+    // if user not found
+    if (!user) {
+      return res.status(404).json({ Error: "User does not exist." });
+    }
+
+    // Validate password ✅
+    const validPw = await bcrypt.compare(password, user.password);
+    if (validPw) {
+      // Create JWT 🥇
+      const token = createToken(user);
+
+      return res.status(200).json({ Message: "Successfully logged in", token });
+    } else {
+      return res.status(404).json({ Error: "Invalid username/password." });
+    }
+  } catch (err) {
+    return next(err);
+  }
 };
 
 module.exports = { signup, login };
