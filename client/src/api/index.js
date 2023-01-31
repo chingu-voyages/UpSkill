f9ec394b5ded6a21ec9c6a6d88b95d1809f8da1f
Mark Linn <marktlinn@gmail.com>
2023-01-31 10:59:46 +0100

    chore: remove dotenv file from package json

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,11 +1,11 @@
 import axois from "axios";
 
 const instance = axois.create({
-  baseURL: import.meta.env.VITE_BACKENDURL,
+  baseURL: import.meta.env.VITE_SERVER,
 });
 
-export const loginAPI = (credentials) =>
+export const loginAPI = credentials =>
   instance.post("/auth/login", credentials);
 
-export const signupAPI = (credentials) =>
+export const signupAPI = credentials =>
   instance.post("/auth/signup", credentials);
