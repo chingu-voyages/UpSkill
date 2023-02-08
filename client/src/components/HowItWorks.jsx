652983be34219c513dd46955b41a01cd93b5fa7d
Mark Linn <marktlinn@gmail.com>
2023-02-08 14:36:24 +0100

    fix: adjust styling and fix responsiveness of how it wroks section for Safari and mobile

--- client/src/components/HowItWorks.jsx
+++ client/src/components/HowItWorks.jsx
@@ -4,14 +4,14 @@ import stepThree from "../assets/landing/step_3.jpg";
 
 const HowItWorks = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col justify-center items-center lg:h-screen">
       <div>
         <h1 className="text-center text-2xl font-bold text-grotto-100 mt-10 mb-10 lg:text-4xl">
           How it works
         </h1>
       </div>
-      <div className="flex flex-col justify-center md:flex-col lg:flex-row lg:p-10">
-        <div className="flex flex-col h-screen items-center gap-6 lg:w-1/4 lg:mx-14">
+      <div className="flex flex-col lg:flex lg:flex-row lg:justify-end ">
+        <div className="flex flex-col items-center gap-6 max-[450px]:min-h-[550px] min-h-[600px] lg:w-1/3">
           <span
             className="font-bold
           text-grotto-100 border-solid border-2 border-slate-300 rounded-full p-2 inline-block w-10 text-center"
@@ -22,12 +22,12 @@ const HowItWorks = () => {
             Find or become a tutor
           </span>
           <img className="max-w-xs" src={stepOne} alt="searching for mentor" />
-          <p className="text-center text-primary leading-6 sm:px-5 lg:text-left lg:w-[300px]">
+          <p className="text-center text-primary leading-6 px-5 lg:text-left max:[400px]:max-w-[350px]">
             Find a tutor, become a tutor for others or do both! Simply search
             for a tutor and book a session.
           </p>
         </div>
-        <div className="flex flex-col h-screen items-center gap-6 lg:w-1/4 lg:mx-14">
+        <div className="flex flex-col items-center gap-6 lg:w-1/3 min-h-[550px]">
           <span
             className="font-bold
           text-grotto-100 border-solid border-2 border-slate-300 rounded-full p-2 inline-block w-10 text-center"
@@ -42,24 +42,28 @@ const HowItWorks = () => {
             src={stepTwo}
             alt="mentor teaching student"
           />
-          <p className="text-center text-primary leading-6 px-5 lg:text-left lg:w-[400px]">
+          <p className="text-center text-primary leading-6 px-5 lg:text-left max:[400px]:max-w-[350px]">
             When you’ve found a tutor book a session. You can either exchange a
             skill you have that your tutor wants to learn or exchange tokens
             that your tutor can use to trade for skills in the future.
           </p>
         </div>
-        <div className="flex flex-col h-screen items-center gap-6 lg:w-1/4 lg:mx-14">
+        <div className="flex flex-col items-center gap-6 lg:w-1/3 min-h-[600px]">
           <span
             className="font-bold
           text-grotto-100 border-solid border-2 border-slate-300 rounded-full p-2 inline-block w-10 text-center"
           >
             3
           </span>
-          <span className="font-bold inline-block text-primary">
+          <span className="font-bold inline-block text-primary z-10">
             Reap the Rewards!
           </span>
-          <img className="max-w-xs" src={stepThree} alt="student celebrating" />
-          <p className="text-center text-primary leading-6 -my-5 px-5 lg:text-left">
+          <img
+            className="max-w-xs lg:-mt-12"
+            src={stepThree}
+            alt="student celebrating"
+          />
+          <p className="text-center text-primary leading-6 -my-5 px-5 lg:text-left max:[400px]:max-w-[350px]">
             Each new member gets free tokens to start booking tuition
             immediately. You too can earn tokens by sharing your skills: learn
             and never spend a penny!
